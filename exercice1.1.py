8285eca490bf4cdc8908972b4c9eaa655c152d41
Nunustolemybike <36703935+Nunustolemybike@users.noreply.github.com>
2020-05-09 10:48:20 +0200

    First breakpoint

---
+++ exercice1.1.py
@@ -0,0 +1,7 @@
+temps=6.92
+distance=19.7
+vitesse= distance/temps
+print(temps)
+print(vitesse)
+print(distance)
+print("%.1f" % vitesse)
---
+++ exercice1.2.py
@@ -0,0 +1,4 @@
+name=str(input("entrez le nom"))
+age=int(input("entrez l'age"))
+print(name)
+print(age)
